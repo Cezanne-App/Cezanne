ac4335ec314f0d4b4f408e7040ad4ea88b397cb7
Sebastian Coronel <scoronelhamilton@gmail.com>
2020-01-15 16:20:04 -0500

    Set up express server to serve static files

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan  = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// Serve Static Files
+app.use(express.static(__dirname + '/../client/dist'));
+
+// Initialization
+const port = process.env.PORT || 3000;
+app.listen(port, (e) => {
+  if (e) {
+    console.error(e);
+  } else {
+    console.log(`Server listening in port ${port}...`);
+  }
+});
