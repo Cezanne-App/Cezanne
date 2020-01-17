7e281f13f275db041388a9c759fe7942fdc1dcef
Sebastian Coronel <scoronelhamilton@gmail.com>
2020-01-17 15:09:13 -0500

    set up env variables for heroku

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/cezanne', {
+const url = process.env.MONGODB_URI || 'mongodb://localhost/cezanne'
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -24,19 +25,3 @@ const artworkSchema = new mongoose.Schema({
 const Artwork = mongoose.model('Artwork', artworkSchema);
 
 module.exports.Artwork = Artwork;
-
-// Objects:
-
-// https://www.harvardartmuseums.org/collections/object/6877?position=19
-// https://www.harvardartmuseums.org/collections/object/262968?position=46
-// https://www.harvardartmuseums.org/collections/object/6108?position=47
-// https://www.harvardartmuseums.org/collections/object/320305?position=12
-
-
-// https://upload.wikimedia.org/wikipedia/commons/d/d4/Paul_C%C3%A9zanne%2C_1892-95%2C_Les_joueurs_de_carte_%28The_Card_Players%29%2C_60_x_73_cm%2C_oil_on_canvas%2C_Courtauld_Institute_of_Art%2C_London.jpg
-
-// Classification
-// Painting
-// Drawing
-// Print
-// Photograph
