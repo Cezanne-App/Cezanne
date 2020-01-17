692853aea7e1a9a4f7503f01ee5a91b8fefacdb6
Sebastian Coronel <scoronelhamilton@gmail.com>
2020-01-16 23:28:41 -0500

    Set front end design

--- client/src/components/post.jsx
+++ client/src/components/post.jsx
@@ -8,20 +8,16 @@ const Post = ({ artWork, setBidArtwork, setModalIsOpen }) => {
   }
   return (
     <div className='post'>
-      <article>
-      <header>{artWork.author}</header>
-      <div>
-        <img src={artWork.image} height='100px' width='100px'/>
+      <div className='post-user-header'>{artWork.author}</div>
+      <div className='post-img-container'><img className='post-image' src={artWork.image} height='100px' width='100px'/></div>
+      <div className='post-info-container'>
+        <div className='post-info'>
+          <h4>{artWork.title}</h4>
+          <span>{`${artWork.technique}, ${artWork.height} x ${artWork.width}`}</span>
+          <span className='post-prices'>{`Base: $${artWork.basePrice}${artWork.highestBid === null ? '' : ` Highest: $${artWork.highestBid}`}`}</span>
+        </div>
+        <div className='post-bid-button'><button onClick={handleBidClick}><img className='post-icon' src='./images/post_icon.png' height='30px' width='30px'/></button></div>
       </div>
-      <div>
-        <h4>{artWork.title}</h4>
-        <span>{artWork.technique} | </span><span>{artWork.height} x {artWork.width}</span>
-      </div>
-      <div>
-        <span>${artWork.basePrice}</span><span>{artWork.highestBid}</span><span>{artWork.expirationDate}</span>
-        <button onClick={handleBidClick}>Bid!</button>
-      </div>
-      </article>
     </div>
   );
 };
