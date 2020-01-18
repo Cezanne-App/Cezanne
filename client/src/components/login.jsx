fd30ef2923624ba68d7e17a3acfc642570565454
Sebastian Coronel <scoronelhamilton@gmail.com>
2020-01-18 16:41:45 -0500

    create login page

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,19 +1,20 @@
 import React, {useState} from 'react';
 
 const Login = ({ setLogged, setUser }) => {
-  const [lastUser, setLastUser] = useState(localStorage.getItem('lastUser'));
   const [attemptedUser, setAttemptedUser] = useState(null);
+  const [attemptedPassword, setAttemptedPassword] = useState(null);
+  const lastUser = localStorage.getItem('lastUser');
   
   const authenticateUser = () => {
-    if (attemptedUser) { // TODO: AUTHENTICATE
+    if (attemptedUser && attemptedPassword) { // TODO: AUTHENTICATE
       setUser(attemptedUser);
       setLogged(true);
+      localStorage.setItem('lastUser', attemptedUser);
     } else {
       console.error('INCORRECT USER');
     }
-    localStorage.setItem('lastUser', attemptedUser);
   };
-
+  
   const handleFormSubmit = (e) => {
     e.preventDefault();
     authenticateUser();
@@ -21,16 +22,21 @@ const Login = ({ setLogged, setUser }) => {
 
   return (
     <div id='login-page' >
-      <div id='login-form'>
-        <h1 className='form-header'>Log in to Cezanne</h1>
-        <form id='log_in' onSubmit={(e) => handleFormSubmit(e)}>
-          <input list='users' type='text' placeholder='Username' onChange={({target}) => setAttemptedUser(target.value)}/>
-          <datalist id="users">
-            <option value={!lastUser ? '' : lastUser}/>
-          </datalist>
-          <input type='text' placeholder='Password'/>
-        </form>
-        <button form='log_in'>Log In</button>
+      <div id='login-img-panel'>
+        <img id='login-img' src='./images/login_img.jpg' />
+      </div>
+      <div id='login-form-container'>
+        <div id='login-form-elements'>
+          <h1 className='form-header'>Log in to Cezanne</h1>
+          <form id='login-form' onSubmit={(e) => handleFormSubmit(e)}>
+            <input list='users' type='text' placeholder='Username' onChange={({target}) => setAttemptedUser(target.value)}/>
+            <datalist id="users">
+              <option value={!lastUser ? '' : lastUser}/>
+            </datalist>
+            <input type='password' placeholder='Password' autoComplete='password' onChange={({target}) => setAttemptedPassword(target.value)}/>
+          </form>
+          <button form='login-form'>Log In</button>
+        </div>
       </div>
     </div>
   );
