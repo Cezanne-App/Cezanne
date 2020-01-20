95dfeaeec68539111079c82f6f5403afe3569fac
Sebastian Coronel <scoronelhamilton@gmail.com>
2020-01-19 23:33:03 -0500

    Add expanded image modal

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -4,12 +4,15 @@ import Navbar from './navbar/navbar.jsx';
 import Feed from './feed.jsx';
 import BidModal from './bid/bidModal.jsx'
 import AddModal from './navbar/addModal.jsx';
+import ImageModal from './post/imageModal.jsx';
 import { getArtworks } from '../../helpers/index.js';
 
 const App = () => {
   const [addModalIsOpen, setAddModalIsOpen] = useState(false);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [bidArtwork, setBidArtwork] = useState(null);
+  const [bidModalIsOpen, setBidModalIsOpen] = useState(false);
+  const [imageModalIsOpen, setImageModalIsOpen] = useState(false);
+
+  const [artwork, setArtwork] = useState(null);
   const [artworks, setArtworks] = useState([]);
 
   const [user, setUser] = useState(null);
@@ -25,9 +28,10 @@ const App = () => {
     return (
       <>
         <Navbar setAddModalIsOpen={setAddModalIsOpen} user={user}/>
-        <Feed artworks={artworks} setBidArtwork={setBidArtwork} setModalIsOpen={setModalIsOpen}/>
-        <BidModal bidArtwork={bidArtwork} setArtworks={setArtworks} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />
+        <Feed artworks={artworks} setArtwork={setArtwork} setModalIsOpen={setBidModalIsOpen} setImageModalIsOpen={setImageModalIsOpen}/>
+        <BidModal bidArtwork={artwork} setArtworks={setArtworks} modalIsOpen={bidModalIsOpen} setModalIsOpen={setBidModalIsOpen} />
         <AddModal addModalIsOpen={addModalIsOpen} setAddModalIsOpen={setAddModalIsOpen} setArtworks={setArtworks}/>
+        <ImageModal artwork={artwork} modalIsOpen={imageModalIsOpen} setModalIsOpen={setImageModalIsOpen}/>
       </>
     );
   };
